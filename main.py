47daaf2d78a0cf357323153b72cdfcf47532c419
Jeff07k <jabejarano09@ucatolica.edu.co>
2025-12-05 08:41:08 -0500

    prepare to commit - front - html.

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Bienvenido a sigmotoa FC {name}"}
+
+#RE-3-FORMMULARARIO RIVAL
+@app.get("/rivales/{equipo_id}")
+async def get_rivals(equipo_id: int ):
+    rivales = {
+        1: ["VISISTANTE"],
+        2: ["LOCAL"],
+    }
+    return {"equipo_id": equipo_id, "rivales": rivales.get(equipo_id, [])}
